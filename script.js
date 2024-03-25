a5852ad661d65a3d94517f14c0542926aca6766e
asobien810 <128296538+asobien810@users.noreply.github.com>
2024-03-25 18:46:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const menuButton1 = document.getElementById('menu-btn1');
+const menuItems1 = document.getElementById('menu-items1')
+const closeButton = document.getElementById('close-btn');
+closeButton.addEventListener("click", close)
+ 
+function toggle() {
+    menuItems1.style.display = "block";
+
+}
+
+function close() {
+    menuItems1.style.display = "none"
+}
+
+
+const menuButton2 = document.getElementById('menu-btn2')
+const menuItems2 = document.getElementById('menu-items2');
+const closeButton2 = document.getElementById('close-btn2')
+function toggle2() {
+    menuItems2.style.display = "block";
+}
+function close2() {
+    menuItems2.style.display = "none"
+}
+
+const menuButton3 = document.getElementById('menu-btn3')
+const menuItems3 = document.getElementById('menu-items3');
+const closeButton3 = document.getElementById('close-btn3')
+function toggle3() {
+    menuItems3.style.display = "block";
+}
+function close3() {
+    menuItems3.style.display = "none"
+}
+
+const menuButton4 = document.getElementById('menu-btn4')
+const menuItems4 = document.getElementById('menu-items4');
+const closeButton4 = document.getElementById('close-btn4')
+function toggle4() {
+    menuItems4.style.display = "block";
+}
+function close4() {
+    menuItems4.style.display = "none"
+}
+
+const menuButton5 = document.getElementById('menu-btn5')
+const menuItems5 = document.getElementById('menu-items5');
+const closeButton5 = document.getElementById('close-btn5')
+function toggle5() {
+    menuItems5.style.display = "block";
+}
+function close5() {
+    menuItems5.style.display = "none"
+}
